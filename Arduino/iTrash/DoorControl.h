816b60e6b1640e4263a18521c7355350eed14e75
daniconde <daniabc2@gmail.com>
2016-04-02 18:36:30 +0200

    Add door control. NO COMPILA

---
+++ Arduino/iTrash/DoorControl.h
@@ -0,0 +1,24 @@
+#ifndef DOOR_CONTROL_
+#define DOOR_CONTROL_
+
+#include "arduino.h"
+#include <Servo.h>
+
+//Bisagra derecha,    pin 8
+//Bisagra izquierda,  pin 9
+class DoorControl {
+  private:
+  
+    Servo left;
+    Servo right;
+  
+  public:
+  
+    DoorControl();
+  
+    void open();
+    
+    void close();
+};
+
+#endif
