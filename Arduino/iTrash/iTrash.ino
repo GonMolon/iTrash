bcd78539ce2f6239f3f86b48a959ee16428d0b79
Gonzalo Solera <gonzalo1796@gmail.com>
2016-04-03 11:42:36 +0200

    FINAL COMMITgit add -A

--- Arduino/iTrash/iTrash.ino
+++ Arduino/iTrash/iTrash.ino
@@ -1,19 +1,16 @@
 #include "Scanner.h"
 #include "ServerComm.h"
 #include "ProximitySensor.h"
-#include "DoorControl.h"
 #include <SPI.h>
 #include <Ethernet.h>
-#include <Servo.h>
 
-#define MIN_LIGHT 930
+#define MIN_LIGHT 950
 #define TRASH_LEVEL_2 20
 #define TRASH_LEVEL_3 15
 
 Scanner scanner;
 ServerComm serverComm;
 ProximitySensor prox_trash(6, 7);
-DoorControl door;
 
 bool trash_open = false;
 
@@ -21,13 +18,14 @@ void setup() {
   Serial.begin(9600);
   scanner.setup();
   serverComm.setup();
-  door.close();
   for(int i = 0; i < 3; ++i) {
       pinMode(32+i*6, OUTPUT);
       pinMode(32+i*6+1, OUTPUT);
       digitalWrite(32+i*6, HIGH);
       digitalWrite(32+i*6+1, LOW);
   }
+  pinMode(8, OUTPUT);
+  digitalWrite(8, LOW);
 }
 
 void loop() {
@@ -42,12 +40,12 @@ void loop() {
     int light = analogRead(A0);
     if(light <= MIN_LIGHT && !trash_open) {
         Serial.println("open door");
-        door.open();
+        digitalWrite(8, HIGH);
         trash_open = true;
     }
     if(light > MIN_LIGHT && trash_open) {
-        Serial.println("open closed");
-        door.close();
+        Serial.println("close door");
+        digitalWrite(8, LOW);
         trash_open = false;
     }
     if(!trash_open) {
